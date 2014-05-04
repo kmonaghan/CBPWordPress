8a22c083fe19bcad9920ae809fe2b0aa9592d064
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-05-04 20:01:23 +0100

    Putting all the headers in the one file

---
+++ CBPWordPress/CBPWordPress.h
@@ -0,0 +1,20 @@
+//
+//  CBPWordPress.h
+//  
+//
+//  Created by Karl Monaghan on 04/05/2014.
+//
+//
+
+#import "CBPWordPressAPIClient.h"
+
+#import "CBPWordPressAttachment.h"
+#import "CBPWordPressAuthor.h"
+#import "CBPWordPressCategory.h"
+#import "CBPWordPressComment.h"
+#import "CBPWordPressImage.h"
+#import "CBPWordPressPost.h"
+#import "CBPWordPressPostContainer.h"
+#import "CBPWordPressTag.h"
+
+#import "NSURLSessionDataTask+CBPWordPress.h"
