1d9a9e39661e4436f49b347b4470977b60f86286
Karl Monaghan <karl.t.monaghan@gmail.com>
2014-07-22 19:52:34 +0100

    Using auto layout for the error and loading views

--- Example/CBPWordPressExample/Controllers/CBPTableViewController.m
+++ Example/CBPWordPressExample/Controllers/CBPTableViewController.m
@@ -30,6 +30,63 @@
     [self.view addSubview:self.loadingView];
     
     [self.view addSubview:self.tableView];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.errorView
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.errorView
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.errorView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.errorView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.loadingView
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.loadingView
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.loadingView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0f
+                                                           constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.loadingView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0f
+                                                           constant:0]];
 }
 
 - (void)viewDidLoad
@@ -42,7 +99,7 @@
                                                object:nil];
     
     __weak typeof(self) weakSelf = self;
-
+    
     if (self.canPullToRefresh) {
         // setup pull-to-refresh
         [self.tableView addPullToRefreshWithActionHandler:^{
@@ -68,6 +125,11 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self
                                                     name:UIContentSizeCategoryDidChangeNotification
                                                   object:nil];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    self.errorLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.view.frame) - (CBPPadding * 2);
 }
 
 #pragma mark -
@@ -123,7 +185,8 @@
 - (UILabel *)errorLabel
 {
     if (!_errorLabel) {
-        _errorLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame) - (CBPPadding * 2), 0)];
+        _errorLabel = [UILabel new];
+        _errorLabel.translatesAutoresizingMaskIntoConstraints = NO;
         _errorLabel.numberOfLines = 0;
         _errorLabel.textAlignment = NSTextAlignmentCenter;
         _errorLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.view.frame) - (CBPPadding * 2);
@@ -136,11 +199,30 @@
 - (UIView *)errorView
 {
     if (!_errorView) {
-        _errorView = [[UIView alloc] initWithFrame:self.tableView.frame];
+        _errorView = [UIView new];
+        _errorView.translatesAutoresizingMaskIntoConstraints = NO;
         _errorView.backgroundColor = [UIColor whiteColor];
         
         [_errorView addSubview:self.errorLabel];
         [_errorView addSubview:self.reloadButton];
+        
+        NSDictionary *views = @{@"errorLabel": self.errorLabel,
+                                @"reloadButton": self.reloadButton};
+        [_errorView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[errorLabel]-(40)-[reloadButton]"
+                                                                           options:NSLayoutFormatAlignAllCenterX
+                                                                           metrics:nil
+                                                                             views:views]];
+        [_errorView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-(padding)-[errorLabel]-(padding)-|"
+                                                                           options:0
+                                                                           metrics:@{@"padding": @(CBPPadding)}
+                                                                             views:views]];
+        [_errorView addConstraint:[NSLayoutConstraint constraintWithItem:self.errorLabel
+                                                               attribute:NSLayoutAttributeCenterY
+                                                               relatedBy:NSLayoutRelationEqual
+                                                                  toItem:_errorView
+                                                               attribute:NSLayoutAttributeCenterY
+                                                              multiplier:1.0f
+                                                                constant:0]];
     }
     
     return _errorView;
@@ -149,14 +231,29 @@
 - (UIView *)loadingView
 {
     if (!_loadingView) {
-        _loadingView = [[UIView alloc] initWithFrame:self.tableView.frame];
+        _loadingView = [UIView new];
+        _loadingView.translatesAutoresizingMaskIntoConstraints = NO;
         _loadingView.backgroundColor = [UIColor whiteColor];
-        
         UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-        spinner.center = _loadingView.center;
+        spinner.translatesAutoresizingMaskIntoConstraints = NO;
         [spinner startAnimating];
         
         [_loadingView addSubview:spinner];
+        
+        [_loadingView addConstraint:[NSLayoutConstraint constraintWithItem:spinner
+                                                                 attribute:NSLayoutAttributeCenterX
+                                                                 relatedBy:NSLayoutRelationEqual
+                                                                    toItem:_loadingView
+                                                                 attribute:NSLayoutAttributeCenterX
+                                                                multiplier:1.0f
+                                                                  constant:0]];
+        [_loadingView addConstraint:[NSLayoutConstraint constraintWithItem:spinner
+                                                                 attribute:NSLayoutAttributeCenterY
+                                                                 relatedBy:NSLayoutRelationEqual
+                                                                    toItem:_loadingView
+                                                                 attribute:NSLayoutAttributeCenterY
+                                                                multiplier:1.0f
+                                                                  constant:0]];
     }
     
     return _loadingView;
@@ -166,16 +263,12 @@
 {
     if (!_reloadButton) {
         _reloadButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _reloadButton.translatesAutoresizingMaskIntoConstraints = NO;
         [_reloadButton addTarget:self action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
         [_reloadButton setTitle:NSLocalizedString(@"Reload", nil) forState:UIControlStateNormal];
         [_reloadButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         
         [_reloadButton sizeToFit];
-        
-        _reloadButton.frame = CGRectMake((CGRectGetWidth(self.view.frame) - CGRectGetWidth(_reloadButton.frame)) / 2,
-                                         CGRectGetHeight(self.view.frame) - CGRectGetHeight(_reloadButton.frame) - 150.0f,
-                                         CGRectGetWidth(_reloadButton.frame),
-                                         CGRectGetHeight(_reloadButton.frame));
     }
     
     return _reloadButton;
